79ce1219a2324e17d9581ba351605b7d1a07bbdd
expwtln <115532482+exponentional-watermelon@users.noreply.github.com>
2024-11-28 22:31:56 +0300

    Update response.js

--- game/js/response.js
+++ game/js/response.js
@@ -1,4 +1,4 @@
-const url = 'https://2bf6-85-198-104-83.ngrok-free.app';
+const url = 'https://7e0a-85-198-104-83.ngrok-free.app';
 
 async function makeResponse(url, data) {
     const response = await fetch(url, {
